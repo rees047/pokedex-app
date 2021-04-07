69e9607bdba03b1264cbc98d355c0ce34fb3e04b
Rees <40927062+rees047@users.noreply.github.com>
2021-04-07 01:51:21 -0400

    Fixed index and Added content to JS
    Fixed styles.css link in index.html
    Add recipeList object array to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,37 @@
-alert("Hello World!");
+let recipeList = [];
 
-let favoriteFood = "spaghetti";
+recipeList = [
+    {
+        recipe : "Spaghetti with Meatballs",
+        mealTimeType : "Appetizer",
+        cuisineType: "Italian",
+        mainIngredient: "Pasta"
+    },
+   {
+        recipe : "Smoked Salmon",
+        mealTimeType : "Dinner",
+        cuisineType: "Around the World",
+        mainIngredient: "Salmon"
+    }, 
+    {
+        recipe : "California Rolls",
+        mealTimeType : "Appetizer",
+        cuisineType: "Japanese",
+        mainIngredient: "Rice"
+    }, 
+    {
+        recipe : "Dumplings",
+        mealTimeType : "Appetizer",
+        cuisineType: "Asian",
+        mainIngredient: "Flour"
+    }, 
+    {
+        recipe : "New York Cheesecake",
+        mealTimeType : "Dessert",
+        cuisineType: "American",
+        mainIngredient: "Cream Cheese"
+    }, 
 
-document.write(favoriteFood);
+]
+
+console.log(recipeList);
